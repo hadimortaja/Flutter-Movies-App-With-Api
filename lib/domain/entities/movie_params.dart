c44d00e63fc39dfc8bb13e026734212557aba906
hadimortaja <hadimortaja9@gmail.com>
2021-03-03 22:02:26 +0200

    simple update

---
+++ lib/domain/entities/movie_params.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class MovieParams extends Equatable {
+  final int id;
+
+  MovieParams(this.id);
+
+  @override
+  List<Object> get props => [id];
+
+}
