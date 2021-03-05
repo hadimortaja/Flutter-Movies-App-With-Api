72cf969ff689555a09edf702709feed3af288831
hadimortaja <hadimortaja9@gmail.com>
2021-03-05 20:37:17 +0200

    Simple Update

--- lib/data/core/api_constants.dart
+++ lib/data/core/api_constants.dart
@@ -3,4 +3,5 @@ class ApiConstants {
   static const String BASE_URL = "https://api.themoviedb.org/3/";
   static const String API_KEY = "a76a130164d24c71bf5ab829183bf343";
   static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-}
+  }
+ 
